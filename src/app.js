9df69ebc39d5f94994a09da458180876b4b08614
TekniqueJoseph <tekniquejoseph@gmail.com>
2023-08-26 13:12:13 -0400

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 const button = document.querySelector('button');
+const textParagraph = document.querySelector('p')
 
 button.addEventListener('click', () => {
-  // do something...
-  const div = document.createElement('div')
-  const h1 = document.createElement('h1')
-  const text = document.createTextNode('Good Morning Dawn. Sing Bella! Sing.')
-  h1.appendChild(text)
-  div.appendChild(h1)
-  document.body.appendChild(div)
+  const text = textParagraph.textContent; // store content of p elements text
+    if (navigator.clipboard) { // feature detection - if 'falsey' fallback code is executed
+      navigator.clipboard.writeText(text) // clipboard API returns a promise
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    } else { // fallback code if feature (navigator API) is unavilable
+      alert('Feature not available...please copy manually!');
+    }
 });
